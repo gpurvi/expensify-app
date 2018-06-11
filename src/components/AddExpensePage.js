9fd73d47ca9a0fa7cb9526309107ebcbe6f1c653
gatis <gatispurvins@inbox.lv>
2018-06-11 18:38:49 +0300

    add styles

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -9,13 +9,20 @@ export class AddExpensePage extends React.Component {
         this.props.startAddExpense(expense);
         this.props.history.push('/');
     };
+
     render() {
         return (
             <div>
-                <h1>Add expense</h1>
-                <ExpenseForm
-                    onSubmit={this.onSubmit}
-                />
+                <div className="page-header">
+                    <div className="content-container">
+                        <h1 className="page-header__title">Add expense</h1>
+                    </div>
+                </div>
+                <div className="content-container">
+                    <ExpenseForm
+                        onSubmit={this.onSubmit}
+                    />
+                </div>
             </div>
         );
     }
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import selectTotalExpenses from '../selectors/expenses-total';
 
 export const ExpensesSummary = (props) => {
     const length = props.expenses.length;
-    const formatTotal = (selectTotalExpenses(props.expenses)/100).toFixed(2);
+    const formatTotal = (selectTotalExpenses(props.expenses) / 100).toFixed(2);
     const includeS = length <= 1 ? '' : 's';
-    return (<h1>Viewing {length} expense{includeS} totalling ${formatTotal}</h1>);
+    return (
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{length}</span> expense{includeS} totalling <span>${formatTotal}</span>
+                </h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 const mapStateToProps = (state) => ({
